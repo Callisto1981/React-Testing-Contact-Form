97d469dba5757938a826b386f31ed6086b8f4141
Wayne <wabbruscatoorr@gmail.com>
2020-06-05 14:24:52 -0400

    updating app test file

--- src/App.test.js
+++ src/App.test.js
@@ -4,4 +4,13 @@ import App from "./App";
 
 test("renders App without crashing", () => {
   const { getByText } = render(<App />);
+
+  const headerText = getByText(/email/i);
+  expect(headerText).toBeVisibile();
+});
+
+test("test first name", () => {
+  const { getByLabelText } = render(<App />);
+  const head = getByLabelText(/first name/i);
+  expect(head).toBeVisible();
 });
